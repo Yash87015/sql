db15ba646ec45e3260b569d90a1d76447892810c
yash solanki <yash87015@gmail.com>
2026-01-11 02:13:16 +0530

    Create app.py

---
+++ tennish_group_project/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import sqlite3 as sql
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
+
+# Connect to the database
+conn = sql.connect('tennish.db')
+
+st.title('Food Waste Management Dashboard')
+
+# --- Dataset Information ---
+st.header('Dataset Information')
